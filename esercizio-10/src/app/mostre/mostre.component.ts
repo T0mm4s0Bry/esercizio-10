82919fee7cd6b955939385b13159c1f01d8cb8a0
T0mm4s0Bry brignoli <brignoli.tommaso@iisgalvanimi.edu.it>
2024-04-23 15:38:59 +0000

    finito tutto

--- esercizio-10/src/app/mostre/mostre.component.ts
+++ esercizio-10/src/app/mostre/mostre.component.ts
@@ -15,9 +15,15 @@ export class MostreComponent {
 
   identificatore: any;
   nome:any;
-  artista: any;
   descrizione:any;
+  descrizione2:any
   date:any;
+  immagine1:any;
+  immagine2:any;
+  opera:any;
+  descrizioneopera:any;
+  opera2:any;
+  descrizioneopera2:any;
 
   constructor(private provaService: ServizioService, private route: ActivatedRoute){
  
@@ -26,9 +32,16 @@ export class MostreComponent {
 
     this.identificatore = this.route.snapshot.paramMap.get('id');
     this.nome = this.provaService.dati[this.identificatore].nome
-    this.artista = this.provaService.dati[this.identificatore].artista
     this.descrizione = this.provaService.dati[this.identificatore].descrizione
+    this.descrizione2 = this.provaService.dati[this.identificatore].descrizione2
     this.date = this.provaService.dati[this.identificatore].date
+    this.immagine1 = this.provaService.dati[this.identificatore].immagine1
+    this.immagine2 = this.provaService.dati[this.identificatore].immagine2
+    this.opera = this.provaService.dati[this.identificatore].opera
+    this.descrizioneopera = this.provaService.dati[this.identificatore].descrizioneopera
+    this.opera2 = this.provaService.dati[this.identificatore].opera2
+    this.descrizioneopera2 = this.provaService.dati[this.identificatore].descrizioneopera2
+
   }
  
 }
